afafe8f45a28942aa429264be0cfe1f945afea59
Grant Croft <grantcroft.25@gmail.com>
2023-12-04 00:06:32 -0500

    final code

---
+++ kalmanFilter_hc-sr04_ESP32_arduinoFramework/include/variables.h
@@ -0,0 +1,41 @@
+
+// Settings:
+bool useKalmanFilter = true;        // Variable to control whether to use the Kalman filter
+const int cmORin = 1;               // 0 for inches, 1 for cm
+const int num_sensors = 3;
+const int num_leds = 4;
+
+
+// Constants:
+const int min_distance[2] = {1, 2}; // Minimum distance in inches/cm
+const int max_distance[2] = {160, 400}; // Maximum distance in inches/cm
+const char* units[2] = {"(inch)", "(cm)"};
+
+
+// Pin out************************************************************
+int servoPin = 33;
+const int ledPins[4] = {19, 21, 22, 23};        
+// 2D array to store [trig, echo] pins of three ultrasonic sensors
+const int ultrasonic[3][2] = {
+  {13, 9},
+  {14, 12},
+  {26, 27}
+};
+// ********************************************************************
+
+
+
+// Objects *************************
+Servo myservo;                              // create servo object to control a servo
+KalmanFilter KalmanFilter(2, 2, 0.01);      // Measurement err, estimate err, process noise
+// *********************************
+
+
+
+// To send the data back after the loop as an array********************************
+unsigned long startTime;  // Variable to store the start time of each phase
+unsigned long elapsedTime; // Variable to store the elapsed time
+double measurements[1000];  // Adjust the size as needed
+int measurementsIndex = 0;
+// *********************************************************************************
+
--- kalmanFilter_hc-sr04_ESP32_arduinoFramework/src/main.cpp
+++ kalmanFilter_hc-sr04_ESP32_arduinoFramework/src/main.cpp
@@ -1,38 +1,36 @@
 /* 
+ * This code is for the ESP32 microcontroller. 
+ * It uses 3 HC-SR04 ultrasonic sensors to measure distance and then uses a Kalman filter to smooth the measurements.
+ * The distance is then displayed using 4 LEDs and a servo motor.
+ 
  * HC-SR04 has range of 2-400 cm. Baud rate of 9600.
- */
+ 
+ * Possible PWM GPIO pins on the ESP32: 0(used by on-board button),2,4,5(used by on-board LED),12-19,21-23,25-27,32-33 
+ * Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
+ * Possible PWM GPIO pins on the ESP32-S3: 0(used by on-board button),1-21,35-45,47,48(used by on-board LED)
+ * Possible PWM GPIO pins on the ESP32-C3: 0(used by on-board button),1-7,8(used by on-board LED),9-10,18-21
 
 
+ * GPOI that dont work for led: 35, 34, 39, 36, 1
+ * GPOI that work for led: 32, 33, 25,      22,23,21, 19, 18 ,26, 27, 14, 12, 13, 9
+
+*/
+
+
+
+// Libraries used++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// The ESP32Servo library is from: https://registry.platformio.org/libraries/madhephaestus/ESP32Servo
+#include <ESP32Servo.h>   // projects/kalmanFilter_with_hc-sr04/kalmanFilter_hc-sr04_ESP32_arduinoFramework/.pio/libdeps/denky32/ESP32Servo/examples
 #include <Arduino.h>
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// My Kalman filter
 #include "KalmanFilter.h"
 
-// Settings:
-bool useKalmanFilter = true;        // Variable to control whether to use the Kalman filter
-const int cmORin = 0;               // 0 for inches, 1 for cm
-const int num_sensors = 3;
 
-
-// Constants:
-const int min_distance[2] = {1, 2}; // Minimum distance in inches/cm
-const int max_distance[2] = {160, 400}; // Maximum distance in inches/cm
-const char* units[2] = {"(inch)", "(cm)"};
-
-// 2D array to store [trig, echo] pins of three ultrasonic sensors
-const int ultrasonic[3][2] = {
-  {13, 9},
-  {27, 14},
-  {32, 33}
-};
-
-
-KalmanFilter KalmanFilter(2, 2, 0.01);      // Measurement err, estimate err, process noise
-
-
-// To send the data back after the loop as an array
-unsigned long startTime;  // Variable to store the start time of each phase
-unsigned long elapsedTime; // Variable to store the elapsed time
-double measurements[1000];  // Adjust the size as needed
-int measurementsIndex = 0;
+// All of my variables
+#include "variables.h"
 
 
 
@@ -43,6 +41,19 @@ void setup() {
     pinMode(ultrasonic[i][0], OUTPUT);
     pinMode(ultrasonic[i][1], INPUT);
   }
+  for (int i = 0; i < num_leds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+
+	// Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 500, 2500); 	// default min/max of 1000us and 2000us
+
 
   Serial.begin(9600);
 }
@@ -82,7 +93,10 @@ void sendMeasurements(double measurements[], int size) {
 }
 
 
-
+void moveServo(double distance, double in_min, long in_max, long out_min, long out_max) {
+  int position = map(distance, in_min, in_max, out_min, out_max);
+  myservo.write(position);
+}
 
 
 // Main loop:
@@ -93,6 +107,7 @@ void loop() {
   startTime = millis();
   elapsedTime = 0;
   measurementsIndex = 0;
+  int position;   // to move the servo
 
 
   // Run the loop for 10 seconds then send the measurements back to the computer for analysis
@@ -147,7 +162,25 @@ void loop() {
     Serial.print("    elapsedTime (ms): ");
     Serial.println(elapsedTime);
 
-    delay(10);
+
+
+
+
+    // Display the distance using the LED
+    // loop through ledPins and turn on the leds depending on the distance. turn one more led on for each 10 units
+    for (int i = 0; i < num_leds; i++) {
+      if (aggregateD > 20 * (i + 1)) {
+        digitalWrite(ledPins[i], HIGH);
+      }
+      else {
+        digitalWrite(ledPins[i], LOW);
+      }
+    }
+
+    // Move the servo depending on the distance
+    moveServo(aggregateD, min_distance[cmORin], 80, 0, 70);       // This is just for visualisation really
+
+    delay(100);   // make sure the servo has moved
     elapsedTime = millis() - startTime;
   }
 
